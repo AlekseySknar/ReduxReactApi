bd9e218e2fd39b994bf66a6942d21a4b23f65828
AlekseySknar <59883118+AlekseySknar@users.noreply.github.com>
2020-02-03 15:25:37 +0700

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import "./styles.css";
-import PhoneAdded from "./components/phoneAdded";
-import PhonesList from "./components/phonesList";
+
 import MainContainer from "./components/mainContainer";
 
 export default function App() {
   return (
     <div className="App">
-      <PhoneAdded />
-      <PhonesList />
+      <MainContainer />
     </div>
   );
 }
--- src/components/mainContainer.tsx
+++ src/components/mainContainer.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import PhoneAdded from "./phoneAdded";
+import PhonesList from "./phonesList";
 
 class MainContainer extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hi</h1>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <PhoneAdded />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PhonesList />
+          </Grid>
+        </Grid>
       </div>
     );
   }
