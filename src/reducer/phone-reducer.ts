0a83f5168b40e76c0e6edc2a1874913036305e2d
AlekseySknar <59883118+AlekseySknar@users.noreply.github.com>
2020-02-03 16:47:31 +0700

    Go home

--- src/reducer/phone-reducer.ts
+++ src/reducer/phone-reducer.ts
@@ -1,4 +1,5 @@
 import * as types from "../actions/action-types";
+import _ from "lodash";
 
 const initialState = {
   phones: [],
@@ -7,3 +8,25 @@ const initialState = {
     model: ""
   }
 };
+
+const userReducer = function(state = initialState, action) {
+  switch (action.type) {
+    case types.GET_PHONES_SUCCESS:
+      return Object.assign({}, state, { users: action.phones });
+
+    case types.DELETE_PHONES_SUCCESS:
+      // Use lodash to create a new user array without the user we want to remove
+      const newUsers = _.filter(
+        state.phone,
+        phone => phone.id != action.phoneId
+      );
+      return Object.assign({}, state, { users: newUsers });
+
+    case types.ADD_PHONES_SUCCESS:
+      return Object.assign({}, state, { phoneProfile: action.userProfile });
+  }
+
+  return state;
+};
+
+export default userReducer;
