b18f4613bc368ae1f49c8d4125bc6e6eeae012e7
AlekseySknar <59883118+AlekseySknar@users.noreply.github.com>
2020-02-03 15:55:03 +0700

    Done UI

--- src/components/phonesList.tsx
+++ src/components/phonesList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const phones = [
   { phone: "Meizu", model: "Pro 7 Plus" },
@@ -7,10 +9,29 @@ const phones = [
 ];
 
 const phonesList = phones.map((value, index) => (
-  <div key={index}>
-    <h2>{value.phone}</h2>
-    <h2>{value.model}</h2>
-  </div>
+  <Grid container spacing={1}>
+    <Grid item xs={5} key={index}>
+      <TextField
+        id="standard-basic"
+        label={value.phone}
+        fullWidth={true}
+        disabled
+      />
+    </Grid>
+    <Grid item xs={5} key={index}>
+      <TextField
+        id="standard-basic"
+        label={value.model}
+        fullWidth={true}
+        disabled
+      />
+    </Grid>
+    <Grid item xs={2} key={index}>
+      <Button variant="contained" fullWidth={true}>
+        Уд.
+      </Button>
+    </Grid>
+  </Grid>
 ));
 
 class PhonesList extends React.Component {
