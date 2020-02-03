cff0a03c70dfc77c42747745be8a3fda781eb7ce
AlekseySknar <59883118+AlekseySknar@users.noreply.github.com>
2020-02-03 13:50:32 +0700

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import "./styles.css";
+import ButtonAppBar from "./components/header";
 
 export default function App() {
   return (
     <div className="App">
+      <ButtonAppBar />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
